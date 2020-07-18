d0785c6e3ab0dc3de2d19c6e16d8313fe6c5325a
Fidel Huarcaya <fidelhuarcaya@gmail.com>
2020-07-18 16:32:24 -0500

    Se crea las los Jpanel dentro de Jframe registro

---
+++ src/exceptions/MyException.java
@@ -0,0 +1,10 @@
+
+package exceptions;
+
+public class MyException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public MyException(String message) {
+		super(message);
+	}
+}
